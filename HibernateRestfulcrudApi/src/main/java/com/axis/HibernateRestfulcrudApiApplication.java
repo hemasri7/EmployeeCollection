8f0f8bf9b6050a923f51612e57846b354a82be5c
hemasri7 <100147870+hemasri7@users.noreply.github.com>
2022-12-09 09:02:53 +0530

    Add files via upload

---
+++ HibernateRestfulcrudApi/src/main/java/com/axis/HibernateRestfulcrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.axis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateRestfulcrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateRestfulcrudApiApplication.class, args);
+	}
+
+}
---
+++ HibernateRestfulcrudApi/src/main/java/com/axis/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.axis.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.axis.entity.User;
+@Repository
+
+public interface UserRepository extends JpaRepository<User,Long> {
+	
+
+}
